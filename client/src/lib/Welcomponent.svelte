a800f860204f27df7609aba38907ff4accc0aba5
Attila Szőke <clownface68@gmail.com>
2022-02-14 22:52:35 +0100

    Added SMUI with light and dark mode themes. Added Button, FormField, Layout Grid and TextField. Added sample button for the sweet sweet champagne.

--- client/src/lib/Welcomponent.svelte
+++ client/src/lib/Welcomponent.svelte
@@ -1,9 +1,18 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Button, { Label } from '@smui/button';
+
+	let clicked = 0;
+</script>
 
 <h1 class="blue">Welcome to SvelteKit</h1>
 <p class="red">
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<Button on:click={() => clicked++}>
+	<Label>Clickmeeee</Label>
+</Button>
+<pre class="status">Clicked: {clicked}</pre>
 
 <style lang="scss">
 	@import '../style/app.scss';
