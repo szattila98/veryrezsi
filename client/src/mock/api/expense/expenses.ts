3a2ce8dd56c58fd8504dc7d18339cfb5a7db72cb
Szőke Attila <clownface68@gmail.com>
2022-04-10 14:53:42 +0200

    Linting.

--- client/src/mock/api/expense/expenses.ts
+++ client/src/mock/api/expense/expenses.ts
@@ -3,8 +3,7 @@ import { response } from '../_common/axios_response';
 import expenses from '$mock/entities/user_expense.json';
 import recurrenceTypes from '$mock/entities/recurrence_type.json';
 import currencyTypes from '$mock/entities/currency_type.json';
-import transactionsJson from "$mock/entities/transaction.json";
-
+import transactionsJson from '$mock/entities/transaction.json';
 
 import type {
 	DeleteTransactionRequestData,
@@ -18,15 +17,15 @@ import type {
 let transactions: Transaction[] | null = null;
 
 const mockGetExpenses = (data: GetExpensesRequestData): GetExpensesResponse => {
-	loadTransactions()
+	loadTransactions();
 
-	if (!data.userId) { 
+	if (!data.userId) {
 		return response(400, []) as GetExpensesResponse;
 	}
 
 	const userExpenses = expenses
 		.filter((expense) => expense.user_id === data.userId)
-		.map((expense) => {  
+		.map((expense) => {
 			return {
 				id: expense.id,
 				name: expense.name,
@@ -41,35 +40,39 @@ const mockGetExpenses = (data: GetExpensesRequestData): GetExpensesResponse => {
 				startDate: expense.startDate,
 				value: expense.value,
 				userId: expense.user_id,
-				transactions: transactions?.filter((transaction) => expense.id === transaction.expenseId)
+				transactions: transactions?.filter((transaction) => expense.id === transaction.expenseId),
 			} as Expense;
-		});  
+		});
 	return response(200, userExpenses) as GetExpensesResponse;
 };
 
 const mockDeleteTransaction = (data: DeleteTransactionRequestData): DeleteTransactionResponse => {
 	if (transactions) {
-		const toDeleteIndex = transactions.findIndex(transaction => transaction.id === data.transactionId)
+		const toDeleteIndex = transactions.findIndex(
+			(transaction) => transaction.id === data.transactionId
+		);
 		if (toDeleteIndex === -1) return response(404) as DeleteTransactionResponse;
 		transactions.splice(toDeleteIndex, 1);
 		return response(200) as DeleteTransactionResponse;
 	}
-	console.log("Transactions not initialized!")
+	console.log('Transactions not initialized!');
 	return response(500) as DeleteTransactionResponse;
 };
 
 function loadTransactions() {
 	if (!transactions) {
-		transactions = transactionsJson.map(transaction => {return{
-			id: transaction.id,
-			donorName: transaction.donor_name,
-			currencyType: currencyTypes.find(
-				(currencyType) => currencyType.id === transaction.currency_type_id
-			),
-			value: transaction.value,
-			date: transaction.date,
-			expenseId: transaction.expense_id,
-		} as Transaction})
+		transactions = transactionsJson.map((transaction) => {
+			return {
+				id: transaction.id,
+				donorName: transaction.donor_name,
+				currencyType: currencyTypes.find(
+					(currencyType) => currencyType.id === transaction.currency_type_id
+				),
+				value: transaction.value,
+				date: transaction.date,
+				expenseId: transaction.expense_id,
+			} as Transaction;
+		});
 	}
 }
 
