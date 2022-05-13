10238dcffcf6caf5c71d11c280050b759136df18
szattila98 <clownface68@gmail.com>
2022-05-14 00:01:36 +0200

    Started diesel orm setup

---
+++ server/init.sql
@@ -0,0 +1,3 @@
+CREATE USER veryrezsi WITH PASSWORD 'veryrezsi';
+CREATE DATABASE veryrezsi;
+GRANT ALL PRIVILEGES ON DATABASE veryrezsi TO veryrezsi;
---
+++ server/src/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    users (id) {
+        id -> Int4,
+        email -> Text,
+        username -> Text,
+        pw_hash -> Text,
+    }
+}
