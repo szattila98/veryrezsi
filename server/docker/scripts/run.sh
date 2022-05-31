8ca23b7d34133df78627cd59f173b6dbbba1f66f
Szőke Attila <clownface68@gmail.com>
2022-05-31 22:48:39 +0200

    Followed with docker builds and dev containers, it is all coming together hopefully.

--- server/docker/scripts/run.sh
+++ server/docker/scripts/run.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-cargo watch --why -x run
+#!/bin/sh
+cd ../../app
+cargo install cargo-watch
+cargo watch --why -x run &
+child=$!
+
+shutdown() {
+    kill $child
+}
+
+trap 'shutdown' TERM
+
+wait $child
