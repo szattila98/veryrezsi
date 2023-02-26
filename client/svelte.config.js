3a8ca88e1a4607edd94197e44f43fe37eddebacd
Attila Szőke <attila.szoke@shiwaforce.com>
2023-02-26 10:26:27 +0100

    Updated project structure

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -10,13 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		alias: { // $lib alias is built-in
-			"$shared": "src/shared",
-			"$shared/*": "src/shared/*",
-			"$api": "src/routes/api",
-			"$api/*": "src/routes/api/*"
-		}
+		adapter: adapter()
 	}
 };
 
