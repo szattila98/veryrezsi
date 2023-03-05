bc8f9d3f334ae8824b04752ccfda6a63e7d82599
Attila Szőke <clownface68@gmail.com>
2023-03-05 12:44:53 +0100

    Created a svell header and footer with icons

---
+++ client/src/lib/components/layout-base/NavEntry.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let text: string;
+	export let href: string;
+	export let iconSize = 6;
+</script>
+
+<a {href} class="flex flex-row align-middle">
+	<div class={`mr-2 h-${iconSize} w-${iconSize}`}>
+		<slot />
+	</div>
+	<span class={`${$$props.class}`}>{text}</span>
+</a>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
+	import Footer from '$lib/components/layout-base/Footer.svelte';
+	import Header from '$lib/components/layout-base/Header.svelte';
 	import '../app.css';
 </script>
 
-<slot />
+<div class="flex min-h-screen flex-col">
+	<Header />
+	<main class="flex-grow">
+		<slot />
+	</main>
+	<Footer />
+</div>
