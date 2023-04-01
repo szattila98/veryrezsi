3dc17b16fdb935be68139a90528404e92b3f8534
Javorek Dénes <javorek.denes@gmail.com>
2023-04-01 09:46:31 +0200

    fix: ci

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build:ci && npm run preview',
 		port: 4173
 	},
 	testDir: 'tests'
