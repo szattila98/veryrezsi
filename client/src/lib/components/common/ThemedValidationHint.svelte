532a1c6212cf0554ec73acfa60144f74bfe5d8ce
Javorek Dénes <35934971+djavorek@users.noreply.github.com>
2023-06-22 18:01:40 +0200

    chore: using hintprops to for themed validation (#78)

--- client/src/lib/components/common/ThemedValidationHint.svelte
+++ client/src/lib/components/common/ThemedValidationHint.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import type { HintProps } from 'svelte-use-form/components/Hint.svelte';
 	import { Hint } from 'svelte-use-form';
 	import IoIosArrowUp from 'svelte-icons/io/IoIosArrowUp.svelte';
 
 	export let msg: string;
-	export let hintProps: object = {};
+	export let hintProps: HintProps = {};
 </script>
 
 <Hint {...hintProps} class="alert variant-ghost-warning rounded-tl-none rounded-tr-none p-2">
